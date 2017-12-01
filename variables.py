e18db5e9be7f7301d7a3e979504891f488afd33d
Brian Akers <brianakers@Brians-MacBook-Pro.local>
2017-12-01 15:02:19 -0500

    variables in Python

---
+++ variables.py
@@ -0,0 +1,20 @@
+# printing to console
+print "We're in Python!"
+
+# setting variable
+test_variable= 4
+print test_variable
+
+# booleans; note that "True" must be upercase
+my_boolean= True
+
+#variables can be reassigned
+test_variable= 7
+print test_variable
+
+#tab indents 4 spaces; must maintain consistency with 2 or 4 spaces
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
